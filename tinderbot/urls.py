89be21bf97f4474af24ff8a445b49d4ed8a6d619
Ulrich <uhoungbo@gmail.com>
2025-01-19 21:45:23 +0100

    add url

--- tinderbot/urls.py
+++ tinderbot/urls.py
@@ -19,5 +19,33 @@ Including another URLconf
 from django.urls import path
 
 urlpatterns = [
-    #    path("admin/", admin.site.urls),
+    path("create-bot-settings/", BotSettingsView.as_view({"patch": "create_settings"})),
+    path(
+        "get-settings",
+        BotSettingsView.as_view({"get": "get_settings"}),
+    ),
+    path(
+        "get-settings-strategy/<uuid:strategy_id>",
+        BotSettingsView.as_view({"get": "get_settings_strategy"}),
+    ),
+    path(
+        "update-bot-settings/<uuid:bot_settings_id>",
+        BotSettingsView.as_view({"patch": "update_settings"}),
+    ),
+    path(
+        "delete-bot-settings/<uuid:bot_settings_id>",
+        BotSettingsView.as_view({"delete": "delete_settings"}),
+    ),
+    path(
+        "start-bot/<uuid:bot_settings_id>",
+        BotSettingsView.as_view({"patch": "start_bot"}),
+    ),
+    path(
+        "stop-bot/<uuid:bot_settings_id>",
+        BotSettingsView.as_view({"patch": "stop_bot"}),
+    ),
+    path(
+        "get-token/",
+        BotSettingsView.as_view({"get": "get_tinder_auth_token"}),
+    ),
 ]
