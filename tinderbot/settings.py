8f98a6774f0995640b1db20dd82d9a216c58040f
Ulrich <uhoungbo@gmail.com>
2025-01-20 13:15:26 +0100

    add url

--- tinderbot/settings.py
+++ tinderbot/settings.py
@@ -74,6 +74,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = "tinderbot.urls"
